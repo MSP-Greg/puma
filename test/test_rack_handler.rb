fc0d6d877c375539791d3810a247f312a4aa4676
MSP-Greg <Greg.mpls@gmail.com>
2023-03-04 21:38:23 -0600

    Update test_rack_handler.rb

--- test/test_rack_handler.rb
+++ test/test_rack_handler.rb
@@ -327,7 +327,7 @@ module TestRackUp
       assert_includes log, 'Puma version'
       assert_includes log, 'Use Ctrl-C to stop'
     ensure
-      return if skipped
+      return if skipped?
       if Puma::IS_WINDOWS
         `taskkill /F /PID #{pid}`
       else
