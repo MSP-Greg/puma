17b9a7745a029f812220abb325f2688f9ca2a4a1
MSP-Greg <Greg.mpls@gmail.com>
2023-03-12 17:57:01 -0500

    helper.rb - add back in ::Timeout.timeout for all platforms

--- test/helper.rb
+++ test/helper.rb
@@ -93,14 +93,19 @@ module TimeoutEveryTestCase
   def run
     with_info_handler do
       time_it do
+        # remove ::Timeout.timeout when (if) tests become stable
         if ::Puma::IS_MRI
           capture_exceptions do
-            before_setup; setup; after_setup
-            self.send self.name
+            ::Timeout.timeout($test_case_timeout, TestTookTooLong) do
+              before_setup; setup; after_setup
+              self.send self.name
+            end
           end
 
           capture_exceptions do
-            Minitest::Test::TEARDOWN_METHODS.each { |hook| self.send hook }
+            ::Timeout.timeout($test_case_timeout, TestTookTooLong) do
+              Minitest::Test::TEARDOWN_METHODS.each { |hook| self.send hook }
+            end
           end
         else
           capture_exceptions do
