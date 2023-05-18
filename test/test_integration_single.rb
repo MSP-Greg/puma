1d220a332cc4ea39a12d1919a1c47be4ed80c512
MSP-Greg <Greg.mpls@gmail.com>
2023-05-18 18:42:19 -0500

    Update test_integration_single.rb

--- test/test_integration_single.rb
+++ test/test_integration_single.rb
@@ -170,7 +170,7 @@ class TestIntegrationSingle < TestIntegration
     stop_server
 
     # macos intermittently raises 'Errno::ENOENT: No such file'
-    sleep 0.25 until File.exist? fn
+    sleep 0.25 unless File.exist? fn
     log = File.read fn
 
     assert_includes log, '"GET / HTTP/1.1"'
@@ -198,7 +198,7 @@ class TestIntegrationSingle < TestIntegration
     stop_server
 
     # macos intermittently raises 'Errno::ENOENT: No such file'
-    sleep 0.25 until File.exist? fn
+    sleep 0.25 unless File.exist? fn
     log = File.read fn
 
     assert_includes log, '"GET / HTTP/1.1"'
