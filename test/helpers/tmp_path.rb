f9322a9fd3a78e8f1a9ee5710692fa1a86caf51f
MSP-Greg <Greg.mpls@gmail.com>
2023-05-18 19:48:29 -0500

    Update tmp_path.rb

--- test/helpers/tmp_path.rb
+++ test/helpers/tmp_path.rb
@@ -23,7 +23,7 @@ module TmpPath
       end
     end
 
-  def tmp_path(extension=nil, unix: false )
+  def tmp_path(extension = nil, unix: false)
     tmp = unix ? PUMA_TMPDIR : nil
     path = Tempfile.create(['', extension], tmp) { |f| f.path }
     tmp_paths << path
