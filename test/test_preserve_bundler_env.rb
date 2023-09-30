36968d7171a2d1b71e15aae6219e1da6f3d87150
MSP-Greg <Greg.mpls@gmail.com>
2023-09-30 16:00:43 -0500

    test_preserve_bundler_env.rb

--- test/test_preserve_bundler_env.rb
+++ test/test_preserve_bundler_env.rb
@@ -17,7 +17,7 @@ class TestPreserveBundlerEnv < TestIntegration
   def test_usr2_restart_preserves_bundler_environment
     skip_unless_signal_exist? :USR2
 
-    @tcp_port = UniquePort.call
+    @bind_port = new_port
     env = {
       # Intentionally set this to something we wish to keep intact on restarts
       "BUNDLE_GEMFILE" => "Gemfile.bundle_env_preservation_test",
@@ -25,24 +25,24 @@ class TestPreserveBundlerEnv < TestIntegration
       "BUNDLER_ORIG_BUNDLE_GEMFILE" => nil
     }
     # Must use `bundle exec puma` here, because otherwise Bundler may not be defined, which is required to trigger the bug
-    cmd = "bundle exec puma -q -w 1 --prune-bundler -b tcp://#{HOST}:#{@tcp_port}"
+    cmd = "bundle exec puma -q -w 1 --prune-bundler -b #{bind_uri_str}"
     Dir.chdir(File.expand_path("bundle_preservation_test", __dir__)) do
       @server = IO.popen(env, cmd.split, "r")
     end
     wait_for_server_to_boot
     @pid = @server.pid
-    connection = connect
-    initial_reply = read_body(connection)
+    socket = send_http
+    initial_reply = socket.read_body
     assert_match("Gemfile.bundle_env_preservation_test", initial_reply)
-    restart_server connection
-    new_reply = read_body(connection)
+    restart_server socket
+    new_reply = socket.read_body
     assert_match("Gemfile.bundle_env_preservation_test", new_reply)
   end
 
   def test_worker_forking_preserves_bundler_config_path
     skip_unless_signal_exist? :TERM
 
-    @tcp_port = UniquePort.call
+    @bind_port = new_port
     env = {
       # Disable the .bundle/config file in the bundle_app_config_test directory
       "BUNDLE_APP_CONFIG" => "/dev/null",
@@ -50,43 +50,41 @@ class TestPreserveBundlerEnv < TestIntegration
       "BUNDLE_GEMFILE" => nil,
       "BUNDLER_ORIG_BUNDLE_GEMFILE" => nil
     }
-    cmd = "bundle exec puma -q -w 1 --prune-bundler -b tcp://#{HOST}:#{@tcp_port}"
+    cmd = "bundle exec puma -q -w 1 --prune-bundler -b #{bind_uri_str}"
     Dir.chdir File.expand_path("bundle_app_config_test", __dir__) do
       @server = IO.popen(env, cmd.split, "r")
     end
     wait_for_server_to_boot
     @pid = @server.pid
-    reply = read_body(connect)
+    reply = send_http_read_resp_body
     assert_equal("Hello World", reply)
   end
 
   def test_phased_restart_preserves_unspecified_bundle_gemfile
     skip_unless_signal_exist? :USR1
 
-    @tcp_port = UniquePort.call
+    @bind_port = new_port
     env = {
       "BUNDLE_GEMFILE" => nil,
       "BUNDLER_ORIG_BUNDLE_GEMFILE" => nil
     }
     set_release_symlink File.expand_path("bundle_preservation_test/version1", __dir__)
-    cmd = "bundle exec puma -q -w 1 --prune-bundler -b tcp://#{HOST}:#{@tcp_port}"
+    cmd = "bundle exec puma -q -w 1 --prune-bundler -b #{bind_uri_str}"
     Dir.chdir(current_release_symlink) do
       @server = IO.popen(env, cmd.split, "r")
     end
     wait_for_server_to_boot
     @pid = @server.pid
-    connection = connect
 
     # Bundler itself sets ENV['BUNDLE_GEMFILE'] to the Gemfile it finds if ENV['BUNDLE_GEMFILE'] was unspecified
-    initial_reply = read_body(connection)
+    initial_reply = send_http_read_resp_body
     expected_gemfile = File.expand_path("bundle_preservation_test/version1/Gemfile", __dir__).inspect
     assert_equal(expected_gemfile, initial_reply)
 
     set_release_symlink File.expand_path("bundle_preservation_test/version2", __dir__)
     start_phased_restart
 
-    connection = connect
-    new_reply = read_body(connection)
+    new_reply = send_http_read_resp_body
     expected_gemfile = File.expand_path("bundle_preservation_test/version2/Gemfile", __dir__).inspect
     assert_equal(expected_gemfile, new_reply)
   end
