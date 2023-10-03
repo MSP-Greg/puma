f74eb20c05ad66fbf1131b76a06a4cfc7e77ac10
MSP-Greg <Greg.mpls@gmail.com>
2023-10-03 08:43:46 -0500

    test_web_server.rb - use PumaSocket

--- test/test_web_server.rb
+++ test/test_web_server.rb
@@ -3,6 +3,7 @@
 # Copyright (c) 2005 Zed A. Shaw
 
 require_relative "helper"
+require_relative "helpers/test_puma/puma_socket"
 
 require "puma/server"
 
@@ -19,13 +20,15 @@ end
 class WebServerTest < Minitest::Test
   parallelize_me!
 
+  include TestPuma
+  include TestPuma::PumaSocket
+
   VALID_REQUEST = "GET / HTTP/1.1\r\nHost: www.zedshaw.com\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\n"
 
   def setup
     @tester = TestHandler.new
     @server = Puma::Server.new @tester, nil, {log_writer: Puma::LogWriter.strings}
-    @port = (@server.add_tcp_listener "127.0.0.1", 0).addr[1]
-    @tcp = "http://127.0.0.1:#{@port}"
+    @bind_port = (@server.add_tcp_listener HOST, 0).addr[1]
     @server.run
   end
 
@@ -34,22 +37,19 @@ class WebServerTest < Minitest::Test
   end
 
   def test_simple_server
-    hit(["#{@tcp}/test"])
+    send_http_read_response GET_10
     assert @tester.ran_test, "Handler didn't really run"
   end
 
   def test_requests_count
     assert_equal @server.requests_count, 0
-    3.times do
-      hit(["#{@tcp}/test"])
-    end
+    3.times { send_http_read_response GET_10 }
     assert_equal @server.requests_count, 3
   end
 
   def test_trickle_attack
-    socket = do_test(VALID_REQUEST, 3)
-    assert_match "hello", socket.read
-    socket.close
+    response = do_test(VALID_REQUEST, 3)
+    assert_includes response,"hello"
   end
 
   def test_close_client
@@ -59,15 +59,13 @@ class WebServerTest < Minitest::Test
   end
 
   def test_bad_client
-    socket = do_test("GET /test HTTP/BAD", 3)
-    assert_match "Bad Request", socket.read
-    socket.close
+    response = do_test("GET /test HTTP/BAD", 3)
+    assert_includes response, "Bad Request"
   end
 
   def test_bad_path
-    socket = do_test("GET : HTTP/1.1\r\n\r\n", 3)
-    assert_match "HTTP/1.1 400 Bad Request\r\n\r\n", socket.read
-    socket.close
+    response = do_test("GET : HTTP/1.1\r\n\r\n", 3)
+    assert_includes response, "HTTP/1.1 400 Bad Request\r\n"
   end
 
   def test_header_is_too_long
@@ -80,56 +78,49 @@ class WebServerTest < Minitest::Test
   def test_file_streamed_request
     body = "a" * (Puma::Const::MAX_BODY * 2)
     long = "GET /test HTTP/1.1\r\nContent-length: #{body.length}\r\nConnection: close\r\n\r\n" + body
-    socket = do_test(long, (Puma::Const::CHUNK_SIZE * 2) - 400)
-    assert_match "hello", socket.read
-    socket.close
+    response = do_test(long, (Puma::Const::CHUNK_SIZE * 2) - 400)
+    assert_includes response, "hello"
   end
 
   def test_supported_http_method
-    socket = do_test("PATCH www.zedshaw.com:443 HTTP/1.1\r\nConnection: close\r\n\r\n", 100)
-    response = socket.read
-    assert_match "hello", response
-    socket.close
+    response = do_test("PATCH www.zedshaw.com:443 HTTP/1.1\r\nConnection: close\r\n\r\n", 100)
+    assert_includes response, "hello"
   end
 
   def test_nonexistent_http_method
-    socket = do_test("FOOBARBAZ www.zedshaw.com:443 HTTP/1.1\r\nConnection: close\r\n\r\n", 100)
-    response = socket.read
-    assert_match "Not Implemented", response
-    socket.close
+    response = do_test("FOOBARBAZ www.zedshaw.com:443 HTTP/1.1\r\nConnection: close\r\n\r\n", 100)
+    assert_includes response, "Not Implemented"
   end
 
   private
 
   def do_test(string, chunk)
     # Do not use instance variables here, because it needs to be thread safe
-    socket = TCPSocket.new("127.0.0.1", @port);
+    socket = new_socket
     request = StringIO.new(string)
     chunks_out = 0
 
     while data = request.read(chunk)
-      chunks_out += socket.write(data)
+      chunks_out += socket.syswrite(data)
       socket.flush
     end
-    socket
+    socket.read_response
   end
 
   def do_test_raise(string, chunk, close_after = nil)
     # Do not use instance variables here, because it needs to be thread safe
-    socket = TCPSocket.new("127.0.0.1", @port);
+    socket = new_socket
     request = StringIO.new(string)
     chunks_out = 0
 
     while data = request.read(chunk)
-      chunks_out += socket.write(data)
+      chunks_out += socket.syswrite(data)
       socket.flush
       socket.close if close_after && chunks_out > close_after
     end
 
-    socket.write(" ") # Some platforms only raise the exception on attempted write
+    socket << " " # Some platforms only raise the exception on attempted write
     socket.flush
     socket
-  ensure
-    socket.close unless socket.closed?
   end
 end
