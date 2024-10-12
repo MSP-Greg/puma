96b0b3287e9099346c47ef77bb98aa16b4432702
MSP-Greg <Greg.mpls@gmail.com>
2024-10-12 10:21:11 -0500

    [TEMP] server max logging

--- lib/puma/server.rb
+++ lib/puma/server.rb
@@ -69,6 +69,7 @@ module Puma
     #   calling code to not require events.rb.
     #
     def initialize(app, events = nil, options = {})
+      @closed_conections = 0
       @app = app
       @events = events || Events.new
 
@@ -396,6 +397,7 @@ module Puma
         end
 
         graceful_shutdown if @status == :stop || @status == :restart
+STDOUT.syswrite "\n********* @closed_conections #{@closed_conections}  reactor #{@reactor&.selector_size_max}  tp #{pool&.backlog_max}\n"
       rescue Exception => e
         @log_writer.unknown_error e, nil, "Exception handling servers"
       ensure
@@ -468,6 +470,7 @@ module Puma
         @requests_count += 1
         case handle_request(client, requests + 1)
         when false
+          @closed_conections += 1
         when :async
           close_socket = false
         when true
