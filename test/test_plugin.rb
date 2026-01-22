aa12218bff9ea338d0c0419ff5636cca06827776
MSP-Greg <Greg.mpls@gmail.com>
2026-01-22 17:22:40 -0600

    ci: test_plugin - `@control_tcp_port` to `@control_port`

--- test/test_plugin.rb
+++ test/test_plugin.rb
@@ -5,11 +5,11 @@ require_relative "helpers/integration"
 
 class TestPlugin < TestIntegration
   def test_plugin
-    @control_tcp_port = UniquePort.call
+    @control_port = UniquePort.call
 
     Dir.mkdir("tmp") unless Dir.exist?("tmp")
 
-    cli_server "--control-url tcp://#{HOST}:#{@control_tcp_port} --control-token #{TOKEN} test/rackup/hello.ru",
+    cli_server "--control-url tcp://#{HOST}:#{@control_port} --control-token #{TOKEN} test/rackup/hello.ru",
       config: "plugin 'tmp_restart'"
 
     File.open('tmp/restart.txt', mode: 'wb') { |f| f.puts "Restart #{Time.now}" }
