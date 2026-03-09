5c314f340e5b7627dcd85ceabfc350f17b62184d
MSP-Greg <Greg.mpls@gmail.com>
2026-03-08 20:00:51 -0500

    Update ssl/context_builder.rb

--- lib/puma/ssl/context_builder.rb
+++ lib/puma/ssl/context_builder.rb
@@ -1,5 +1,5 @@
-require 'openssl'
-require 'open3'
+# frozen_string_literal: true
+
 
 module Puma
   module SSL
@@ -10,44 +10,63 @@ module Puma
       end
 
       def context
-        ctx = OpenSSL::SSL::SSLContext.new
+        ctx = SSL::Context.new
 
-        key = if params['key']
-                File.open(params['key'])
-              elsif params['key_pem']
-                params['key_pem']
-              else
-                log_writer.error "Please specify the SSL key via 'key=' or 'key_pem='"
-              end
-        ctx.key = OpenSSL::PKey.read(key, key_password(params['key_password_command']))
+        if Puma::IS_JRUBY
+          unless params['keystore']
+            log_writer.error "Please specify the Java keystore via 'keystore='"
+          end
 
-        cert = if params['cert']
-          File.binread(params['cert'])
-        elsif params['cert_pem']
-          params['cert_pem']
+          ctx.keystore = params['keystore']
+
+          unless params['keystore-pass']
+            log_writer.error "Please specify the Java keystore password  via 'keystore-pass='"
+          end
+
+          ctx.keystore_pass = params['keystore-pass']
+          ctx.keystore_type = params['keystore-type']
+
+          if truststore = params['truststore']
+            ctx.truststore = truststore.eql?('default') ? :default : truststore
+            ctx.truststore_pass = params['truststore-pass']
+            ctx.truststore_type = params['truststore-type']
+          end
+
+          ctx.cipher_suites = params['cipher_suites'] || params['ssl_cipher_list']
+          ctx.protocols = params['protocols'] if params['protocols']
         else
-          log_writer.error "Please specify the SSL cert via 'cert=' or 'cert_pem='"
+          if params['key'].nil? && params['key_pem'].nil?
+            log_writer.error "Please specify the SSL key via 'key=' or 'key_pem='"
+          end
+
+          ctx.key = params['key'] if params['key']
+          ctx.key_pem = params['key_pem'] if params['key_pem']
+          ctx.key_password_command = params['key_password_command'] if params['key_password_command']
+
+          if params['cert'].nil? && params['cert_pem'].nil?
+            log_writer.error "Please specify the SSL cert via 'cert=' or 'cert_pem='"
+          end
+
+          ctx.cert = params['cert'] if params['cert']
+          ctx.cert_pem = params['cert_pem'] if params['cert_pem']
+
+          if ['peer', 'force_peer'].include?(params['verify_mode'])
+            unless params['ca']
+              log_writer.error "Please specify the SSL ca via 'ca='"
+            end
+            # needed for Puma::OpenSSL::SSL::Socket#peer_cert, env['puma.peer_cert']
+            require 'openssl'
+          end
+
+          ctx.ca = params['ca'] if params['ca']
+          ctx.ssl_cipher_filter = params['ssl_cipher_filter'] if params['ssl_cipher_filter']
+          ctx.ssl_ciphersuites = params['ssl_ciphersuites'] if params['ssl_ciphersuites'] && HAS_TLS1_3
+
+          ctx.reuse = params['reuse'] if params['reuse']
         end
-        ctx.cert = OpenSSL::X509::Certificate.new(cert)
 
-        #ctx.ssl_cipher_filter = params['ssl_cipher_filter'] if params['ssl_cipher_filter']
-        #ctx.ssl_ciphersuites = params['ssl_ciphersuites'] if params['ssl_ciphersuites'] && HAS_TLS1_3
-
-        #ctx.reuse = params['reuse'] if params['reuse']
-
-        #ctx.no_tlsv1   = params['no_tlsv1'] == 'true'
-        #ctx.no_tlsv1_1 = params['no_tlsv1_1'] == 'true'
-
-        # TODO figure out why params['ca'] is an empty string
-        ca = params['ca'] && !params['ca'].empty? ? params['ca'] : nil
-        if ['peer', 'force_peer'].include?(params['verify_mode']) && !ca
-          log_writer.error "Please specify the SSL ca via 'ca='"
-        end
-        if ca
-          cert_store = OpenSSL::X509::Store.new
-          cert_store.add_file ca
-          ctx.cert_store = cert_store
-        end
+        ctx.no_tlsv1   = params['no_tlsv1'] == 'true'
+        ctx.no_tlsv1_1 = params['no_tlsv1_1'] == 'true'
 
         if params['verify_mode']
           ctx.verify_mode = case params['verify_mode']
@@ -63,11 +82,11 @@ module Puma
                             end
         end
 
-        #if params['verification_flags']
-          #ctx.verification_flags = params['verification_flags'].split(',').
-            #map { |flag| MiniSSL::VERIFICATION_FLAGS.fetch(flag) }.
-            #inject { |sum, flag| sum ? sum | flag : flag }
-        #end
+        if params['verification_flags']
+          ctx.verification_flags = params['verification_flags'].split(',').
+            map { |flag| OpenSSL::SSL::VERIFICATION_FLAGS.fetch(flag) }.
+            inject { |sum, flag| sum ? sum | flag : flag }
+        end
 
         ctx
       end
@@ -75,16 +94,6 @@ module Puma
       private
 
       attr_reader :params, :log_writer
-
-      # Executes the command to return the password needed to decrypt the key.
-      def key_password(command)
-        return nil if command.nil?
-
-        stdout_str, stderr_str, status = Open3.capture3(command)
-        return stdout_str.chomp if status.success?
-
-        raise "Key password failed with code #{status.exitstatus}: #{stderr_str}"
-      end
     end
   end
 end
