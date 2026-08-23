8aa100f2ab51d24ab02e6a0bdde3feb250827435
MSP-Greg <Greg.mpls@gmail.com>
2026-08-23 15:45:54 -0500

    Update integration.rb

--- test/helpers/integration.rb
+++ test/helpers/integration.rb
@@ -42,7 +42,7 @@ class TestIntegration < PumaTest
   end
 
   def teardown
-    err = (Puma::IS_JRUBY || @ignore_stderr || !@server_err&.wait_readable(0.001)) ?
+    err = Puma::IS_JRUBY || @ignore_stderr || !@server_err&.wait_readable(0.001) ?
       nil : @server_err&.read
 
     if @server && !@server_stopped
